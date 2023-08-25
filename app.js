8349e4cef0655396d0ad05b2a75e97fb1466580b
Priyanka Kadukar <77113441+Priyanka-lab460@users.noreply.github.com>
2023-08-25 16:33:22 +0530

    new code changes 1

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-(function() {
+(function(){
     'use strict';
-
     angular.module('LunchCheck', [])
-        .controller('LunchCheckController', LunchCheckController);
-
-    LunchCheckController.$inject = ['$scope'];
-
-    function LunchCheckController($scope) {
-        $scope.dishes = '';
-        $scope.message = '';
-        $scope.checked = false;
-
-        $scope.checkLunch = function() {
-            if ($scope.dishes.trim().length === 0) {
-                $scope.empty = true;
-            } else {
-                $scope.checked = true;
-                $scope.empty = false;
-                var arrayDishes = $scope.dishes.split(',');
-                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
-                    return v.trim() !== '';
-                });
-
-                if (arrayDishesWithoutEmptys.length <= 3) {
-                    $scope.message = 'Enjoy!';
-                } else {
-                    $scope.message = 'Too much!';
-                }
+    .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject=['$scope'];
+    function LunchCheckController($scope){
+        $scope.items="";
+        $scope.message="";
+        $scope.checkLunchMessage={
+            "color": ""
+        }
+        $scope.styleForTextbox={
+            border:""
+        }
+        $scope.checkItems=function(){
+            let tochk=$scope.items.split(',');
+            tochk.sort();
+            let fntochk=tochk.filter(function(item){
+                item=item.replaceAll(' ', '');
+                return item!=="";
+            })
+            console.log("items: "+fntochk+" "+fntochk.length);
+            if(fntochk.length==0){
+                $scope.message="Please enter data first";
+                $scope.checkLunchMessage.color="red";
+                $scope.styleForTextbox.border="1px solid red";
+            } else if(fntochk.length<=3){
+                $scope.message="Enjoy!";
+                $scope.checkLunchMessage.color="green";
+                $scope.styleForTextbox.border="1px solid green";
+            } else{
+                $scope.message="Too much!";
+                $scope.checkLunchMessage.color="green";
+                $scope.styleForTextbox.border="1px solid green";
             }
-        };
+        }
     }
 })();
